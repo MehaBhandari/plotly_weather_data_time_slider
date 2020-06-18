3bfb4cf562f9c493f1c240f3d2ecb781b8e6ef3c
Meha <ahem4progs@gmail.com>
2020-06-18 10:26:06 +0530

    Updated code for multiple lines

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var rawDataURL = 'https://5ee311f28b27f30016095353.mockapi.io/data/prediction';
 var xField = 'Date';
-var yField = 'Mean_TemperatureC';
+var yField = 'Amount';
 
 var selectorOptions = {
     buttons: [{
@@ -28,283 +28,241 @@ var selectorOptions = {
     }],
 };
 
-data1 = [
+data = [
   {
-    "Date": "1/1/1948",
-    "Max_TemperatureC": 10,
-    "Mean_TemperatureC": 8,
-    "Min_TemperatureC": 7
+    "Date": "1/31/2019",
+    "Bank": "Bank of America",
+    "Amount": 80405.84,
+    "Predicted Amount LR": 80405.84,
+    "Predicted Amount MVLR": 80405.84,
+    "Predicted Amount NN": 80405.84,
+    "snMSE LR": null,
+    "snMSE MVLR": null,
+    "snMSE NN": null
   },
   {
-    "Date": "1/2/1948",
-    "Max_TemperatureC": 6,
-    "Mean_TemperatureC": 4,
-    "Min_TemperatureC": 3
+    "Date": "2/28/2019",
+    "Bank": "Bank of America",
+    "Amount": 88655.76,
+    "Predicted Amount LR": 88655.76,
+    "Predicted Amount MVLR": 88655.76,
+    "Predicted Amount NN": 88655.76,
+    "snMSE LR": null,
+    "snMSE MVLR": null,
+    "snMSE NN": null
   },
   {
-    "Date": "1/3/1948",
-    "Max_TemperatureC": 7,
-    "Mean_TemperatureC": 4,
-    "Min_TemperatureC": 2
+    "Date": "4/30/2019",
+    "Bank": "Bank of America",
+    "Amount": 87070.36,
+    "Predicted Amount LR": 87070.36,
+    "Predicted Amount MVLR": 87070.36,
+    "Predicted Amount NN": 87070.36,
+    "snMSE LR": null,
+    "snMSE MVLR": null,
+    "snMSE NN": null
   },
   {
-    "Date": "1/4/1948",
-    "Max_TemperatureC": 7,
-    "Mean_TemperatureC": 4,
-    "Min_TemperatureC": 2
+    "Date": "5/31/2019",
+    "Bank": "Bank of America",
+    "Amount": 92685.28,
+    "Predicted Amount LR": 92685.28,
+    "Predicted Amount MVLR": 92685.28,
+    "Predicted Amount NN": 92685.28,
+    "snMSE LR": null,
+    "snMSE MVLR": null,
+    "snMSE NN": null
   },
   {
-    "Date": "1/5/1948",
-    "Max_TemperatureC": 7,
-    "Mean_TemperatureC": 3,
-    "Min_TemperatureC": 0
+    "Date": "6/30/2019",
+    "Bank": "Bank of America",
+    "Amount": 96670.37,
+    "Predicted Amount LR": 96670.37,
+    "Predicted Amount MVLR": 96670.37,
+    "Predicted Amount NN": 96670.37,
+    "snMSE LR": null,
+    "snMSE MVLR": null,
+    "snMSE NN": null
   },
   {
-    "Date": "1/6/1948",
-    "Max_TemperatureC": 9,
-    "Mean_TemperatureC": 7,
-    "Min_TemperatureC": 5
+    "Date": "7/31/2019",
+    "Bank": "Bank of America",
+    "Amount": 88655.76,
+    "Predicted Amount LR": 88655.76,
+    "Predicted Amount MVLR": 88655.76,
+    "Predicted Amount NN": 88655.76,
+    "snMSE LR": null,
+    "snMSE MVLR": null,
+    "snMSE NN": null
   },
   {
-    "Date": "1/7/1948",
-    "Max_TemperatureC": 10,
-    "Mean_TemperatureC": 7,
-    "Min_TemperatureC": 4
+    "Date": "8/31/2019",
+    "Bank": "Bank of America",
+    "Amount": 81442.62,
+    "Predicted Amount LR": 81442.62,
+    "Predicted Amount MVLR": 81442.62,
+    "Predicted Amount NN": 81442.62,
+    "snMSE LR": null,
+    "snMSE MVLR": null,
+    "snMSE NN": null
   },
   {
-    "Date": "1/8/1948",
-    "Max_TemperatureC": 8,
-    "Mean_TemperatureC": 5,
-    "Min_TemperatureC": 2
+    "Date": "9/30/2019",
+    "Bank": "Bank of America",
+    "Amount": 101804.83,
+    "Predicted Amount LR": 101804.83,
+    "Predicted Amount MVLR": 101804.83,
+    "Predicted Amount NN": 101804.83,
+    "snMSE LR": null,
+    "snMSE MVLR": null,
+    "snMSE NN": null
   },
   {
-    "Date": "1/9/1948",
-    "Max_TemperatureC": 9,
-    "Mean_TemperatureC": 4,
-    "Min_TemperatureC": 1
+    "Date": "10/31/2019",
+    "Bank": "Bank of America",
+    "Amount": 94305.03,
+    "Predicted Amount LR": 94305.03,
+    "Predicted Amount MVLR": 94305.03,
+    "Predicted Amount NN": 94305.03,
+    "snMSE LR": null,
+    "snMSE MVLR": null,
+    "snMSE NN": null
   },
   {
-    "Date": "1/10/1948",
-    "Max_TemperatureC": 6,
-    "Mean_TemperatureC": 4,
-    "Min_TemperatureC": 2
+    "Date": "11/30/2019",
+    "Bank": "Bank of America",
+    "Amount": 87070.36,
+    "Predicted Amount LR": 87070.36,
+    "Predicted Amount MVLR": 87070.36,
+    "Predicted Amount NN": 87070.36,
+    "snMSE LR": null,
+    "snMSE MVLR": null,
+    "snMSE NN": null
   },
   {
-    "Date": "1/11/1948",
-    "Max_TemperatureC": 5,
-    "Mean_TemperatureC": 3,
-    "Min_TemperatureC": 1
-  }
-]
-
-data2 = [
-  {
-    "Date": "1/12/1948",
-    "Max_TemperatureC": 5,
-    "Mean_TemperatureC": 1,
-    "Min_TemperatureC": -2
+    "Date": "12/31/2019",
+    "Bank": "Bank of America",
+    "Amount": 91876.81,
+    "Predicted Amount LR": 91876.81,
+    "Predicted Amount MVLR": 91876.81,
+    "Predicted Amount NN": 91876.81,
+    "snMSE LR": null,
+    "snMSE MVLR": null,
+    "snMSE NN": null
   },
   {
-    "Date": "1/13/1948",
-    "Max_TemperatureC": 7,
-    "Mean_TemperatureC": 3,
-    "Min_TemperatureC": -1
+    "Date": "1/31/2020",
+    "Bank": "Bank of America",
+    "Amount": 99126.12,
+    "Predicted Amount LR": 100825.7905,
+    "Predicted Amount MVLR": 82328.63149,
+    "Predicted Amount NN": 88921.89593,
+    "snMSE LR": 0.014530305,
+    "snMSE MVLR": 0.021127276,
+    "snMSE NN": 0.008660095
   },
   {
-    "Date": "1/14/1948",
-    "Max_TemperatureC": 3,
-    "Mean_TemperatureC": 0,
-    "Min_TemperatureC": -3
+    "Date": "2/29/2020",
+    "Bank": "Bank of America",
+    "Amount": 96670.37,
+    "Predicted Amount LR": 103636.899,
+    "Predicted Amount MVLR": 82723.11151,
+    "Predicted Amount NN": 86163.44406,
+    "snMSE LR": null,
+    "snMSE MVLR": null,
+    "snMSE NN": null
   },
   {
-    "Date": "1/15/1948",
-    "Max_TemperatureC": 1,
-    "Mean_TemperatureC": 1,
-    "Min_TemperatureC": 0
-  },
-  {
-    "Date": "1/16/1948",
-    "Max_TemperatureC": 1,
-    "Mean_TemperatureC": -1,
-    "Min_TemperatureC": -2
-  },
-  {
-    "Date": "1/17/1948",
-    "Max_TemperatureC": 1,
-    "Mean_TemperatureC": 0,
-    "Min_TemperatureC": -1
-  },
-  {
-    "Date": "1/18/1948",
-    "Max_TemperatureC": 1,
-    "Mean_TemperatureC": 0,
-    "Min_TemperatureC": -1
-  },
-  {
-    "Date": "1/19/1948",
-    "Max_TemperatureC": 1,
-    "Mean_TemperatureC": -1,
-    "Min_TemperatureC": -2
-  },
-  {
-    "Date": "1/20/1948",
-    "Max_TemperatureC": 2,
-    "Mean_TemperatureC": 1,
-    "Min_TemperatureC": -1
-  },
-  {
-    "Date": "1/21/1948",
-    "Max_TemperatureC": 8,
-    "Mean_TemperatureC": 4,
-    "Min_TemperatureC": 1
-  },
-  {
-    "Date": "1/22/1948",
-    "Max_TemperatureC": 8,
-    "Mean_TemperatureC": 7,
-    "Min_TemperatureC": 5
-  }
-]
-
-data3 = [
-  {
-    "Date": "7/4/1948",
-    "Max_TemperatureC": 19,
-    "Mean_TemperatureC": 14,
-    "Min_TemperatureC": 11
-  },
-  {
-    "Date": "7/5/1948",
-    "Max_TemperatureC": 19,
-    "Mean_TemperatureC": 15,
-    "Min_TemperatureC": 11
-  },
-  {
-    "Date": "7/6/1948",
-    "Max_TemperatureC": 21,
-    "Mean_TemperatureC": 16,
-    "Min_TemperatureC": 11
-  },
-  {
-    "Date": "7/7/1948",
-    "Max_TemperatureC": 18,
-    "Mean_TemperatureC": 14,
-    "Min_TemperatureC": 11
-  },
-  {
-    "Date": "7/8/1948",
-    "Max_TemperatureC": 22,
-    "Mean_TemperatureC": 16,
-    "Min_TemperatureC": 10
-  },
-  {
-    "Date": "7/9/1948",
-    "Max_TemperatureC": 25,
-    "Mean_TemperatureC": 18,
-    "Min_TemperatureC": 11
-  },
-  {
-    "Date": "7/10/1948",
-    "Max_TemperatureC": 22,
-    "Mean_TemperatureC": 18,
-    "Min_TemperatureC": 14
-  },
-  {
-    "Date": "7/11/1948",
-    "Max_TemperatureC": 22,
-    "Mean_TemperatureC": 17,
-    "Min_TemperatureC": 13
-  },
-  {
-    "Date": "7/12/1948",
-    "Max_TemperatureC": 22,
-    "Mean_TemperatureC": 17,
-    "Min_TemperatureC": 11
-  },
-  {
-    "Date": "7/13/1948",
-    "Max_TemperatureC": 24,
-    "Mean_TemperatureC": 18,
-    "Min_TemperatureC": 11
-  },
-  {
-    "Date": "7/14/1948",
-    "Max_TemperatureC": 25,
-    "Mean_TemperatureC": 18,
-    "Min_TemperatureC": 12
-  },
-  {
-    "Date": "7/15/1948",
-    "Max_TemperatureC": 28,
-    "Mean_TemperatureC": 20,
-    "Min_TemperatureC": 12
-  },
-  {
-    "Date": "7/16/1948",
-    "Max_TemperatureC": 27,
-    "Mean_TemperatureC": 20,
-    "Min_TemperatureC": 14
+    "Date": "3/31/2020",
+    "Bank": "Bank of America",
+    "Amount": 87070.36,
+    "Predicted Amount LR": 106448.0075,
+    "Predicted Amount MVLR": 82706.81369,
+    "Predicted Amount NN": 86163.44406,
+    "snMSE LR": null,
+    "snMSE MVLR": null,
+    "snMSE NN": null
   }
  ]
-
-// Plotly.d3.json(rawDataURL, function(err, rawData) {
-//     debugger;
-//     if(err) throw err;
-
-//     var data = prepData(rawData);
-//     var layout = {
-//         title: 'Time series with range slider and selectors',
-//         xaxis: {
-//             rangeselector: selectorOptions,
-//             rangeslider: {}
-//         },
-//         yaxis: {
-//             fixedrange: true
-//         }
-//     };
-
-//     Plotly.newPlot('graph1', data, layout);
-// });
-
-// Plotly.d3.json(rawDataURL, function(err, rawData) {
-//     debugger;
-//     if(err) throw err;
-
-//     var data = prepData(rawData);
-//     var layout = {
-//         title: 'Time series without range slider and selectors',
-//         xaxis: {
-//             rangeselector: selectorOptions
-//         },
-//         yaxis: {
-//             fixedrange: true
-//         }
-//     };
-
-//     Plotly.newPlot('graph2', data, layout);
-// });
-
+ 
+console.log("data1 length: " + data.length)
+data1 = data.slice(0,11);
+data2 = data.slice(10);
 function prepData(rawData) {
-    var x = [];
-    var y = [];
+  var x = [];
+  var y = [];
+  
+  rawData.forEach(function(datum, i) {
+    x.push(new Date(datum[xField]));
+    y.push(datum[yField]);
+  });
 
-    rawData.forEach(function(datum, i) {
+  return [{
+    mode: 'lines',
+    x: x,
+    y: y
+  }];
+};
 
-        x.push(new Date(datum[xField]));
-        y.push(datum[yField]);
-    });
+yField1 = 'Predicted Amount LR';
+function prepData1(rawData) {
+  var x = [];
+  var y = [];
+  
+  rawData.forEach(function(datum, i) {
+    x.push(new Date(datum[xField]));
+    y.push(datum[yField1]);
+  });
 
-    return [{
-        mode: 'lines',
-        x: x,
-        y: y
-    }];
+  return [{
+    mode: 'lines',
+    x: x,
+    y: y
+  }];
+};
+
+yField2 = 'Predicted Amount MVLR';
+function prepData2(rawData) {
+  var x = [];
+  var y = [];
+  
+  rawData.forEach(function(datum, i) {
+    x.push(new Date(datum[xField]));
+    y.push(datum[yField2]);
+  });
+
+  return [{
+    mode: 'lines',
+    x: x,
+    y: y
+  }];
+};
+
+yField3 = 'Predicted Amount NN';
+function prepData3(rawData) {
+  var x = [];
+  var y = [];
+  
+  rawData.forEach(function(datum, i) {
+    x.push(new Date(datum[xField]));
+    y.push(datum[yField3]);
+  });
+
+  return [{
+    mode: 'lines',
+    x: x,
+    y: y
+  }];
 };
 
 trace1 = prepData(data1);
 trace2 = prepData(data2);
-trace3 = prepData(data3);
-data = [trace1[0], trace2[0], trace3[0]];
+trace3 = prepData1(data2);
+trace4 = prepData2(data2);
+trace5 = prepData3(data2);
+
+// trace3 = prepData(data3);
+data = [trace1[0],trace2[0],trace3[0],trace4[0],trace5[0]];
 var layout = {
     title: 'Time series with range slider and selectors',
     xaxis: {
